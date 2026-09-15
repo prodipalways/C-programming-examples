326f98695af648ee7bfea7e9854a9d59aa33f2d3
Prodip Ray <65656694+prodipalways@users.noreply.github.com>
2026-09-16 00:03:49 +0600

    Update sum of digits using do while loop.c

--- 06. Loop/sum of digits using do while loop.c
+++ 06. Loop/sum of digits using do while loop.c
@@ -1,1 +1,22 @@
 
+#include <stdio.h>
+
+int main()
+{
+    int n, digit, sum = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    do
+    {
+        digit = n % 10;
+        sum = sum + digit;
+        n = n / 10;
+    }
+    while(n > 0);
+
+    printf("Sum of digits = %d", sum);
+
+    return 0;
+}
