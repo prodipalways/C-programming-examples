7c06a0a718b7be3ea7498082030e9dbc9e0ef56c
Prodip Ray <65656694+prodipalways@users.noreply.github.com>
2026-09-16 00:03:04 +0600

    Update sum of digits using for loop.c

--- 06. Loop/sum of digits using for loop.c
+++ 06. Loop/sum of digits using for loop.c
@@ -1,1 +1,20 @@
 
+#include <stdio.h>
+
+int main()
+{
+    int n, digit, sum = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    for(; n > 0; n = n / 10)
+    {
+        digit = n % 10;
+        sum = sum + digit;
+    }
+
+    printf("Sum of digits = %d", sum);
+
+    return 0;
+}
