0270b5a3f2695282eca9db2ea1df6a146d2060a5
Prodip Ray <65656694+prodipalways@users.noreply.github.com>
2026-09-16 00:19:40 +0600

    Update Palindrome Number Use do while Loop.c

--- 06. Loop/Palindrome Number Use do while Loop.c
+++ 06. Loop/Palindrome Number Use do while Loop.c
@@ -1,1 +1,27 @@
 
+#include <stdio.h>
+
+int main()
+{
+    int n, original, reverse = 0, digit;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    original = n;
+
+    do
+    {
+        digit = n % 10;
+        reverse = reverse * 10 + digit;
+        n = n / 10;
+    }
+    while(n > 0);
+
+    if(original == reverse)
+        printf("Palindrome");
+    else
+        printf("Not Palindrome");
+
+    return 0;
+}
