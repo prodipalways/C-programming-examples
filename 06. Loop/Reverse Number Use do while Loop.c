1a7b8be4dd5bbad3ddaa43e26b23a05850c4d8ee
Prodip Ray <65656694+prodipalways@users.noreply.github.com>
2026-09-15 23:57:25 +0600

    Update Reverse Number Use do while Loop.c

--- 06. Loop/Reverse Number Use do while Loop.c
+++ 06. Loop/Reverse Number Use do while Loop.c
@@ -1,1 +1,22 @@
 
+#include <stdio.h>
+
+int main()
+{
+    int n, rev = 0, digit;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    do
+    {
+        digit = n % 10;
+        rev = rev * 10 + digit;
+        n = n / 10;
+    }
+    while(n > 0);
+
+    printf("Reverse = %d", rev);
+
+    return 0;
+}
