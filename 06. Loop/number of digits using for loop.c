13a1c7e62416de2775ed9967ea56f572d98d363d
Prodip Ray <65656694+prodipalways@users.noreply.github.com>
2026-09-16 00:05:33 +0600

    Update number of digits using for loop.c

--- 06. Loop/number of digits using for loop.c
+++ 06. Loop/number of digits using for loop.c
@@ -1,1 +1,19 @@
 
+#include <stdio.h>
+
+int main()
+{
+    int n, count = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    for(; n > 0; n = n / 10)
+    {
+        count++;
+    }
+
+    printf("Number of digits = %d", count);
+
+    return 0;
+}
