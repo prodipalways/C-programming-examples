5024a7718ff5feb979f8f0fc6c584612794b0380
Prodip Ray <65656694+prodipalways@users.noreply.github.com>
2026-09-15 23:52:24 +0600

    Update Reverse Number Use For Loop.c

--- 06. Loop/Reverse Number Use For Loop.c
+++ 06. Loop/Reverse Number Use For Loop.c
@@ -1,1 +1,20 @@
 
+#include <stdio.h>
+
+int main()
+{
+    int n, rev = 0, digit;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    for(; n > 0; n = n / 10)
+    {
+        digit = n % 10;
+        rev = rev * 10 + digit;
+    }
+
+    printf("Reverse = %d", rev);
+
+    return 0;
+}
